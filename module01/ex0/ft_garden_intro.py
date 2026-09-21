fb6fb67a34e59823683eb2cdf945e29f46a0f659
wkf1lthy <habbie7650@icloud.com>
2026-09-21 17:38:12 +0200

    module01

---
+++ module01/ex0/ft_garden_intro.py
@@ -0,0 +1,11 @@
+
+name = "Rose"
+height = "25 cm"
+age = "30 days"
+
+if __name__ == "__main__":
+    print("Welcome to my garden!")
+    print("Plant:", name)
+    print("Height:", height)
+    print("Age:", age)
+    print("End of program")
